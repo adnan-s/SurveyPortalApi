a7f909c6a098528102d4e3da3a6d4f82031e2fa7
adnan-s <adnan.siddiqui@live.co.uk>
2022-11-09 13:53:20 +0000

    adding api tests files

--- test/faqApi.spec.js
+++ test/faqApi.spec.js
@@ -1,13 +1,5 @@
 var assert = require('assert');
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
-    });
-});
-
 describe("FAQ API:", () => {
     it("should bring a list of FAQs", () => {
         const x = 10;
@@ -22,6 +14,3 @@ describe("FAQ API:", () => {
 
     });
 })
-
-
-console.log("Tested!");
